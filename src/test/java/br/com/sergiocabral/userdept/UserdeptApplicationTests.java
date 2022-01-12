73e63971e22c35774b77563da504792252a996d5
Sergio_Cabral <scastrocabral@gmail.com>
2022-01-12 11:40:23 -0300

    Create spring-boot configuration , entities user and department, and maps object relacional

---
+++ src/test/java/br/com/sergiocabral/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sergiocabral.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
